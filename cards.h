0347de4aa2ac6165777a254625bc4948e02ad6e2
James Little <jlittle@my1ps.com>
2023-01-18 09:40:01 +0000

    feat: card generation from header file

---
+++ cards.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <map>
+using std::map;
+using std::pair;
+using std::string;
+using std::vector;
+// vector < map<string<pair<string, int>>> projectCards = {
+vector<map<string, vector<pair<string, int>>>> mainCards = {
+
+    {{"production", {{"megaCredits", 2}}}, {"bank", {{"megaCredits", 2}}}, {"costOffSet", {{"megaCredits", 2}}}}
+
+};
+vector<map<string, vector<pair<string, int>>>> corporationCards = {{{"production", {{"megaCredits", 2}}}, {"bank", {{"megaCredits", 2}}}, {"costOffSet", {{"megaCredits", 2}}}}};
--- main.cpp
+++ main.cpp
@@ -2,11 +2,103 @@
 #include <map>
 #include <string>
 #include <vector>
+#include "cards.h"
 
+using std::cout;
+using std::endl;
 using std::map;
 using std::pair;
 using std::string;
 using std::vector;
+void createDeck();
+enum CardType
+{
+  event,
+  corporate,
+  active,
+  project
+};
+class CardBase
+{
+private:
+  /* data */
+public:
+  string title;
+  string description;
+  CardType type;
+  int victoryPoints = 0;
+  map<string, int> tags = {
+      {"science", 0}, {"building", 0}, {"space", 0}, {"earth", 0}, {"jovian", 0}, {"plant", 0}, {"animal", 0}, {"microbe", 0}
+
+  };
+  map<string, int> production = {
+      {"megaCredits", 0}, {"steel", 0}, {"titanium", 0}, {"plants", 0}, {"energy", 0}, {"heat", 0}
+
+  };
+  map<string, int> bank = {
+      {"megaCredits", 0}, {"steel", 0}, {"titanium", 0}, {"plants", 0}, {"energy", 0}, {"heat", 0}
+
+  };
+  map<string, int> costOffSet = {
+      {"megaCredits", 0}, {"steel", 0}, {"titanium", 0}, {"plants", 0}, {"energy", 0}, {"heat", 0}
+
+  };
+
+  CardBase(map<string, vector<pair<string, int>>> &cardConfig)
+  {
+    for (unsigned short i = 0; auto const &[key, value] : cardConfig)
+    {
+
+      if (key == "production")
+      {
+        for (unsigned short i = 0; auto const &[inKey, inValue] : cardConfig[key])
+        {
+          production[inKey] = production[inKey] + inValue;
+        }
+      }
+      else if (key == "bank")
+      {
+        for (unsigned short i = 0; auto const &[inKey, inValue] : cardConfig[key])
+        {
+          bank[inKey] = bank[inKey] + inValue;
+        }
+      }
+      else if (key == "costOffset")
+      {
+        for (unsigned short i = 0; auto const &[inKey, inValue] : cardConfig[key])
+        {
+          costOffSet[inKey] = costOffSet[inKey] + inValue;
+        }
+      }
+
+      ++i;
+    }
+  };
+  void listCardValues()
+  {
+    for (auto const &[key, val] : production)
+    {
+      cout << "Production " << key // string (key)
+           << ": "
+           << val // string's value
+           << endl;
+    }
+    for (auto const &[key, val] : bank)
+    {
+      cout << "Bank " << key // string (key)
+           << ": "
+           << val // string's value
+           << endl;
+    }
+    for (auto const &[key, val] : costOffSet)
+    {
+      cout << "OffSet " << key // string (key)
+           << ": "
+           << val // string's value
+           << endl;
+    }
+  }
+};
 
 class Bank
 {
@@ -14,12 +106,31 @@ private:
 public:
   map<string, pair<string, int>> bankMap = {
       {"megaCredits", {"Mega Credits", 0}},
-      // {"Steel", 0},
-      // {"Titanium", 0},
-      // {"Plants", 0},
-      // {"Power", 0},
-      // {"Heat", 0}
+      {"energy", {"Energy", 0}},
+      {"steel", {"Steel", 0}},
+      {"titanium", {"Titanium", 0}},
+      {"plants", {"Plants", 0}},
+      {"heat", {"Heat", 0}},
+
   };
+  void updateBank(vector<pair<string, int>> value)
+  {
+    for (size_t i = 0; i < value.size(); i++)
+    {
+
+      bankMap[value[i].first].second = value[i].second;
+    }
+  };
+  void listBank()
+  {
+    for (auto const &[key, val] : bankMap)
+    {
+      cout << val.first // string (key)
+           << ": "
+           << val.second // string's value
+           << endl;
+    }
+  }
 };
 
 class Production
@@ -28,19 +139,19 @@ private:
 public:
   map<string, pair<string, int>> productionMap = {
       {"megaCredits", {"Mega Credits", 0}},
-      // {"Steel", 0},
-      // {"Titanium", 0},
-      // {"Plants", 0},
-      // {"Power", 0},
-      // {"Heat", 0}
+      {"energy", {"Energy", 0}},
+      {"steel", {"Steel", 0}},
+      {"titanium", {"Titanium", 0}},
+      {"plants", {"Plants", 0}},
+      {"heat", {"Heat", 0}},
   };
-  void initProduction(vector<pair<string, int>> corporation)
+  void updateProduction(vector<pair<string, int>> value)
   {
 
-    for (size_t i = 0; i < corporation.size(); i++)
+    for (size_t i = 0; i < value.size(); i++)
     {
 
-      productionMap[corporation[i].first].second = corporation[i].second;
+      productionMap[value[i].first].second = value[i].second;
     }
   }
 
@@ -48,13 +159,49 @@ public:
   {
     for (auto const &[key, val] : productionMap)
     {
-      std::cout << val.first // string (key)
-                << ": "
-                << val.second // string's value
-                << std::endl;
+      cout << val.first // string (key)
+           << ": "
+           << val.second // string's value
+           << endl;
     }
   }
 };
+
+class CostOffset
+{
+private:
+  /* data */
+public:
+  map<string, pair<string, int>> offsetMap = {
+      {"megaCredits", {"Mega Credits", 0}},
+      {"energy", {"Energy", 0}},
+      {"steel", {"Steel", 0}},
+      {"titanium", {"Titanium", 0}},
+      {"plants", {"Plants", 0}},
+      {"heat", {"Heat", 0}},
+  };
+
+  void updateCostOffset(vector<pair<string, int>> value)
+  {
+
+    for (size_t i = 0; i < value.size(); i++)
+    {
+
+      offsetMap[value[i].first].second = value[i].second;
+    }
+  }
+  void listOffset()
+  {
+    for (auto const &[key, val] : offsetMap)
+    {
+      cout << val.first // string (key)
+           << ": "
+           << val.second // string's value
+           << endl;
+    }
+  }
+};
+
 class Player
 {
 private:
@@ -62,15 +209,78 @@ public:
   string name = "";
   Production production;
   Bank bank;
+  CostOffset costOffset;
+  vector<CardBase> playerHand;
+  vector<CardBase> activeEffectCards;
+  vector<CardBase> eventDiscard;
+  vector<CardBase> playedProjects;
+  map<string, int> tags = {
+      {"science", 0}, {"building", 0}, {"space", 0}, {"earth", 0}, {"jovian", 0}, {"plant", 0}, {"animal", 0}, {"microbe", 0}
+
+  };
+  int victoryPoints = 0;
+
+  Player(map<string, vector<pair<string, int>>> &corp)
+  {
+    for (unsigned short i = 0; auto const &[key, value] : corp)
+    {
+
+      if (key == "production")
+      {
+        production.updateProduction(value);
+      }
+      else if (key == "bank")
+      {
+        bank.updateBank(value);
+      }
+      else if (key == "costOffset")
+      {
+        costOffset.updateCostOffset(value);
+      }
+
+      ++i;
+    }
+  }
 };
+vector<CardBase> mainDeck;
+vector<CardBase> corporationDeck;
+vector<CardBase> discardDeck;
 
 int main(int argc, char const *argv[])
 {
-  using std::cout;
-  Player player;
-  vector<pair<string, int>> testCorporation = {{"megaCredits", 42}};
-  player.production.initProduction(testCorporation);
-  player.production.listProducion();
+
+  map<string, vector<pair<string, int>>> testCorporation = {{"production", {{"megaCredits", 2}}}, {"bank", {{"megaCredits", 42}}}, {"costOffset", {{"megaCredits", 1}}}};
+  Player player(testCorporation);
+  createDeck();
+  for (size_t i = 0; i < mainDeck.size(); i++)
+  {
+    mainDeck[i].listCardValues();
+  }
+  for (size_t i = 0; i < corporationDeck.size(); i++)
+  {
+    corporationDeck[i].listCardValues();
+  }
+
+  // CardBase cardTest(testCorporation);
+  // cardTest.listCardValues();
+  // player.production.listProducion();
+  // player.bank.listBank();
+  // player.costOffset.listOffset();
 
   return 0;
+}
+
+void createDeck()
+{
+
+  for (size_t i = 0; i < mainCards.size(); i++)
+  {
+    CardBase newCard(mainCards[i]);
+    mainDeck.push_back(newCard);
+  }
+  for (size_t i = 0; i < corporationCards.size(); i++)
+  {
+    CardBase newCard(corporationCards[i]);
+    corporationDeck.push_back(newCard);
+  }
 }
